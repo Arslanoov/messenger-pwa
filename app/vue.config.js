4dc5a007f1321a0ea233a183250029591669e20a
Arslanoov <rasularslanoov@gmail.com>
2021-05-07 20:48:19 +0500

    build: Removed prefetch

--- app/vue.config.js
+++ app/vue.config.js
@@ -6,6 +6,8 @@ module.exports = {
   chainWebpack: config => {
     const types = ["vue-modules", "vue", "normal-modules", "normal"]
     types.forEach(type => addStyleResource(config.module.rule("stylus").oneOf(type)))
+
+    config.plugins.delete("prefetch")
   },
 
   devServer: {
