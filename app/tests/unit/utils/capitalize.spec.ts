25fdb20fd36a1e29b8c574ce485d3cc1eb65ae7a
Arslanoov <rasularslanoov@gmail.com>
2021-05-29 08:33:04 +0500

    test: Added capitalize util test

---
+++ app/tests/unit/utils/capitalize.spec.ts
@@ -0,0 +1,13 @@
+import { expect } from "chai"
+
+import { capitalize } from "@/utils/capitalize"
+
+describe("capitalize utils", () => {
+  it("works", () => {
+    const name = "vasya"
+    expect(capitalize(name)).to.equal("Vasya")
+
+    const name2 = "ivan"
+    expect(capitalize(name2)).to.equal("Ivan")
+  })
+})
